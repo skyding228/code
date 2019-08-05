030e11164bc0453b2fdbedd1ab256c0be7fd47ee
weichunhe <1329555958@qq.com>
2019-08-05 14:26:43 +0800

    新增缓存功能

--- utils/src/main/java/fun/hereis/code/utils/cache/GuavaCache.java
+++ utils/src/main/java/fun/hereis/code/utils/cache/GuavaCache.java
@@ -5,34 +5,73 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
- * guava 异步刷新
+ * 基于guava cache 封装了一层常用的缓存场景，就是异步全量刷新配置缓存。
+ * 每一种配置创建一个缓存对象，缓存值就是所有的配置项。所以对于guava cache 只有一个key, value; 创建时只需要提供缓存刷新时间，及加载方法即可。
  *
  * @author weichunhe
  * created at 2019/7/22K
  */
 public class GuavaCache<V> {
-
+    /**
+     * 加载缓存接口
+     *
+     * @param <V>
+     */
     public interface Loader<V> {
         V load();
     }
 
+    /**
+     * 缓存加载器
+     */
     private Loader<V> loader;
+    /**
+     * guava 缓存，只有一个key，value
+     */
     private LoadingCache<String, V> cache;
+    /**
+     * 唯一的一个key值
+     */
     private static final String KEY = "CACHE_KEY";
 
+    /**
+     * 获取缓存值
+     *
+     * @return
+     */
     public V get() {
-        return cache.getIfPresent(KEY);
+        try {
+            return cache.get(KEY);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
+    /**
+     * 强制刷新缓存
+     */
+    public void refresh() {
+        try {
+            cache.put(KEY, loader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 构造一个配置缓存
+     *
+     * @param cache
+     * @param loader
+     */
     private GuavaCache(LoadingCache<String, V> cache, Loader<V> loader) {
         this.cache = cache;
-        cache.put(KEY, loader.load());
+        this.loader = loader;
+        refresh();
     }
 
     /**
@@ -49,9 +88,9 @@ public class GuavaCache<V> {
     /**
      * 创建一个异步刷新的缓存,里面只有一个缓存值，直接调用get方法返回数据
      *
-     * @param refreshAfterMinutes
-     * @param loader
-     * @param <V>
+     * @param refreshAfterMinutes 缓存刷新时间间隔，分钟数
+     * @param loader              缓存加载器
+     * @param <V>                 缓存值类型
      * @return
      */
     public static <V> GuavaCache<V> asyncGuavaCache(long refreshAfterMinutes, Loader<V> loader) {
@@ -65,7 +104,6 @@ public class GuavaCache<V> {
     }
 
 
-
     /**
      * 创建一个异步刷新的缓存,key类型为string
      *
@@ -74,21 +112,8 @@ public class GuavaCache<V> {
      * @param <V>
      * @return
      */
-    public static <V> LoadingCache<String, V> asyncRefreshCache( long refreshAfterMinutes, CacheLoader<String, V> loader) {
+    public static <V> LoadingCache<String, V> asyncRefreshCache(long refreshAfterMinutes, CacheLoader<String, V> loader) {
         return CacheBuilder.newBuilder().refreshAfterWrite(refreshAfterMinutes, TimeUnit.MINUTES).build(CacheLoader.asyncReloading(loader, executor));
-    }
-
-
-    /**
-     * 创建一个同步刷新的缓存,key类型为string
-     *
-     * @param refreshAfterMinutes
-     * @param loader
-     * @param <V>
-     * @return
-     */
-    public static <V> LoadingCache<String, V> syncRefreshCache( long refreshAfterMinutes, CacheLoader<String, V> loader) {
-        return CacheBuilder.newBuilder().refreshAfterWrite(refreshAfterMinutes, TimeUnit.MINUTES).build(loader);
     }
 
 }
--- utils/src/test/java/fun/hereis/code/utils/TestGuavaCache.java
+++ utils/src/test/java/fun/hereis/code/utils/TestGuavaCache.java
@@ -25,8 +25,46 @@ public class TestGuavaCache {
         GuavaCache<List<String>> cacheList = GuavaCache.asyncGuavaCache(1, () -> Lists.newArrayList(value));
         Assert.assertEquals("列表缓存失败", value, cacheList.get().get(0));
 
-        GuavaCache<Set<String>> cacheSet = GuavaCache.asyncGuavaCache( 1, () -> Sets.newHashSet(value));
-        Assert.assertTrue("集合缓存失败",cacheSet.get().contains(value));
+        GuavaCache<Set<String>> cacheSet = GuavaCache.asyncGuavaCache(1, () -> Sets.newHashSet(value));
+        Assert.assertTrue("集合缓存失败", cacheSet.get().contains(value));
+
+        cache = GuavaCache.asyncGuavaCache(1, () -> Long.toString(System.currentTimeMillis()));
+        String v = cache.get();
+        System.out.println(v);
+        Assert.assertEquals("初始化失败", v, v);
+        cache.refresh();
+        String v2 = cache.get();
+        Assert.assertTrue("刷新失败", !v.equals(v2));
+
+    }
+
+    @Test
+    public void testAsyncRefresh() {
+        GuavaCache<String> cache = GuavaCache.asyncGuavaCache(1, () -> {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Long.toString(System.currentTimeMillis());
+        });
+        String v = cache.get();
+        System.out.println(v);
+        Assert.assertEquals("初始化失败", v, v);
+        cache.refresh();
+        String v2 = cache.get();
+        System.out.println(v2);
+        Assert.assertTrue("刷新失败", !v.equals(v2));
+    }
+
+    @Test
+    public void testException() {
+        GuavaCache<String> cache = GuavaCache.asyncGuavaCache(1, () -> {
+            throw new RuntimeException("异常了");
+        });
+        String v = cache.get();
+        System.out.println("============="+v);
+        Assert.assertEquals("初始化失败", v, v);
     }
 
 }
