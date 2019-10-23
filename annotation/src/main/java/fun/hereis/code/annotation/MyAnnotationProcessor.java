28760f1316b5f91d3cafe80992bbed12048e0b63
weichunhe <1329555958@qq.com>
2019-10-23 15:59:07 +0800

    注解说明

---
+++ annotation/src/main/java/fun/hereis/code/annotation/MyAnnotationProcessor.java
@@ -0,0 +1,55 @@
+package fun.hereis.code.annotation;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+/**
+ * @author weichunhe
+ * created at 2019/10/23
+ */
+@SupportedAnnotationTypes({"*"})
+public class MyAnnotationProcessor extends AbstractProcessor {
+
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param annotations
+     * @param roundEnv
+     */
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        System.out.println("11111111111111111111111111111111111111111111");
+        log("11111111111======================");
+        return true;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        System.out.println("222222222222222222222222222222222222");
+        log("2222222222======================");
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment env) {
+        super.init(env);
+        System.out.println("=========================================");
+        log("33333333======================");
+        System.out.println(env);
+    }
+
+    private void log(String msg) {
+        this.processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, msg);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("build");
+    }
+}
